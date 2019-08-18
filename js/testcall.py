421cd705e0339c584299a4da144ac03651efb00c
marquisss <32654396+marquisss@users.noreply.github.com>
2019-08-17 19:33:34 -0700

    updating testing code

--- js/testcall.py
+++ js/testcall.py
@@ -1,15 +1,9 @@
 import requests
 
-response = requests.get('http://localhost:3100/create_account',
-    data = {
-        'email' : 'email5',
-        'hashpass' : 'hashedpass',
-        'name' : 'first last',
-        'year' : 1,
-        'isStudentLeader' : False,
-        'gender' : 'Male',
-        'isAdmin' : False
-    }
+response = requests.put('http://localhost:3100/validate_account',
+    data = {'email' : 'email@gpmail.org'}
 )
+
+
 
 print(response.content)
