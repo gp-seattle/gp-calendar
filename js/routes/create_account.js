5023423a655bcb8b2650b98a213f9fa2c177ee91
marquisss <32654396+marquisss@users.noreply.github.com>
2019-08-17 19:34:06 -0700

    finished create_account and started new endpoint

--- js/routes/create_account.js
+++ js/routes/create_account.js
@@ -6,64 +6,44 @@
 //      -how will the code know the password?
 
 
-//encrypts data before storing it in a json
-const cryptoJSON = require('crypto-json')
-
-const fs = require('fs');
-
-var output
 
 //imports express
 const express = require('express')
 
 //acts sort of like a middleware with routing capabilities
 const router = express.Router();
-
+const paramsNeeded = ['email', 'hashpass', 'name', 'year', 'isStudentLeader', 'gender']
+var users = require('./../modules/decryptUsers')
 
 // handle incoming request to /users
-router.all('/', (req, res, next) => {
+router.post('/', (req, res, next) => {
 
   var params = req.body;
 
-  //sets up vars to encrypt data
-  const keys = ['email', 'hashpass', 'name', 'year', 'isStudentLeader', 'isAdmin', 'gender']
-  const algorithm = 'aes256'
-  const encoding = 'hex'
-  var password = require('./../data/password')
-
-  //gets the encrypted user data
-  var encryptUsers = require('./../data/users')
-
-
-  //decrypts the data
-  var users = []
-  for (i = 0; i < encryptUsers.length; i++) {
-    users[i] = cryptoJSON.decrypt(encryptUsers[i], password, algorithm, encoding, keys);
-  }
-
-  console.log(users)
-
   //ensures there are all required keys
   var valid = true;
-  for (key in keys) {
-
-    if (params[keys[key]] == undefined) {
+  for (key in paramsNeeded) {
+    if (params[paramsNeeded[key]] == undefined) {
       valid = false;
     }
   }
 
-
   if (!valid) {     //error for missing keys
-    const error = new Error("Bad Requests");
-    error.status = 403;
-    error.message = "403 Missing required parameter"
-    next(error);   
+    
+      
   } else if (containsEmail(users, params['email'])){     //errors for existing account
     const error = new Error("Bad Requests");
     error.status = 403;
     error.message = "403 Account already exists with given email"
     next(error);    
+  } else if (!params['email'].endsWith('@gpmail.org') && params['isAdmin'] == 'true') {
+    const error = new Error("Bad Requests");
+    error.status = 403;
+    error.message = "403 Email is not valid for an admin account"
+    next(error);   
   } else {     //valid request
+
+    var admin = params['email'].endsWith('@gpmail.org').toString()
 
     //creates new account info
     var newAcc = {
@@ -72,29 +52,22 @@ router.all('/', (req, res, next) => {
       'name' : params['name'],
       'year' : params['year'], 
       'isStudentLeader' : params['isStudentLeader'],
-      'isAdmin' : params['isAdmin'],
-      'gender' : params['gender']
+      'isAdmin' : admin,
+      'gender' : params['gender'],
+      'validated' : "false"
     }
-     
-    password = require('./../modules/genPass')
-    fs.writeFileSync('./data/password.json', JSON.stringify(password));
 
-    console.log(password)
+    users[users.length] = newAcc
 
-    for (i = 0; i < users.length; i++) {
-      output = cryptoJSON.encrypt(
-        newAcc, password, {encoding, keys, algorithm}
-      )
-      encryptUsers[i] = output
-    }
+    var encryptAll = require('./../modules/encryptUsers.js')
+
+    encryptAll(users)
 
     res.status(200)
-    fs.writeFileSync('./data/users.json', JSON.stringify(encryptUsers));
     res.end()
   }
 
-  //writes the encrypted data to the user file
-
+  //checks if email is in container
   function containsEmail(container, email) {
     for (i = 0; i < container.length; i++) {
       if (users[i]['email'] == email) {
@@ -103,11 +76,7 @@ router.all('/', (req, res, next) => {
     }
     return false;
   }
-
 });
-
-
-
 
 
 module.exports = router;
---
+++ js/routes/validateAccount.js
@@ -0,0 +1,28 @@
+//changes the validated value of a user to true when the api is called
+const express = require('express')
+const router = express.Router();
+const encryptUsers = require('./../modules/encryptUsers')
+
+
+router.put('/', (req, res, next) => {
+    var users = require('./../modules/decryptUsers')
+    var done = false
+    for (user in users) {
+        if(users[user]['email'] == req.body['email']) {
+            users[user]['validated'] = "true";
+            done = true
+        }
+    }
+
+    encryptUsers(users)
+    if (done) {
+        res.sendStatus(200);
+    } else {
+        const error = new Error("Bad Requests");
+        error.status = 403;
+        error.message = "403 User not found"
+        next(error); 
+    }
+});
+
+module.exports = router
