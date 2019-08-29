89d1a10ca1338d047f1971107fa30594b72afe4b
marquisss <32654396+marquisss@users.noreply.github.com>
2019-08-29 02:51:24 -0700

    cleaning and adding security for other endpoints

--- js/routes/login/login.js
+++ js/routes/login/login.js
@@ -10,49 +10,70 @@ router.use(cookieParser())
 // handle incoming request to /users
 router.get('/', (req, res, next) => {
     //checks for current session
-    if (req.cookies.seshId) {
+    if (req.cookies.seshId && req.cookies.email) {
+        var sessions = require('./../../modules/encryption/decryptSessions.js')
+        var userSessions = sessions[req.cookies.email]
+
+        //checks if cookies are valid still
+        var valid = false
+        userSessions.forEach(element => {
+            if (element == req.cookies.seshId) {
+                valid = true;
+            }
+        });
+        if (valid) {
+            res.status(200)
+            res.send("Success!")
+        } else {
+            res.status(403)
+            res.send("Invalid cookies")
+        }
 
     } else {
-        var users = require('./../../modules/decryptUsers.js')
+        //imports users
+        var users = require('./../../modules/encryption/decryptUsers.js')
 
+        //checks for required params
         if (!req.body.email) {
             const error = new Error("Bad Requests");
             error.status = 403;
-            error.message = "403 Missing parameter: email";
+            error.message = "403 in login. Missing parameter: email";
             next(error);   
-        }
-        if (!req.body.pass) {
+        } else if (!req.body.pass) {
             const error = new Error("Bad Requests");
             error.status = 403;
-            error.message = "403 Missing parameter: pass";
+            error.message = "403 in login. Missing parameter: pass";
             next(error);   
-        }
-
-
-        var found = false
-        var user = null;
-        for (i = 0; i < users.length && !found; i++) {
-            if (users[i]['email'] == req.body.email) {
-                found = true;
-                user = users[i]
-            }
-        }
-    
-        if (req.body.pass === user['hashpass']) {
-            res.status(200)
-            var makeid = require ('./../../modules/genSeshId.js')
-            res.cookie()
-            res.send("<HTML><body><p>Hello " + user['name'] + "!</p></body></HTML>")
         } else {
-            res.status(403)
-            res.send("Login failed!")
+            //checks that email is valid
+            var user = null;
+            for (i = 0; i < users.length; i++) {
+                if (users[i]['email'] == req.body.email) {
+                    user = users[i]
+                    break;
+                }
+            }
+
+            //generates new session and stores cookies
+            if (req.body.pass === user['hashpass'] && found) {
+                res.status(200)
+                var makeid = require ('./../../modules/encryption/genSeshId.js')
+                var id = makeid(req.body.email)
+                if(user['validated'] == 'true') {
+                    res.cookie('validated', 'valid')
+                }
+                res.cookie('seshId', id)
+                res.cookie('email', req.body.email)
+                res.send("<HTML><body><p>Hello " + user['name'] + "!</p></body></HTML>")
+            } else {
+                //no email found
+                res.status(403)
+                res.send("Login failed!")
+            }
         }
     }
 
 });
 
 
-
-
 module.exports = router;
-
