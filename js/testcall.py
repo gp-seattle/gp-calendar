165e8fd5078085ebbd4a6a4a24bfb6c74d0f9698
marquisss <32654396+marquisss@users.noreply.github.com>
2019-08-29 02:48:14 -0700

    updating testing files

--- js/testcall.py
+++ js/testcall.py
@@ -1,16 +1,49 @@
 import requests
+import time
+s = requests.Session() 
+for i in range(1,45):
+    email = "test" + str(i) + "@gmail.com"
+    response = s.post(
+        'http://localhost:3100/create_account',
+        data = {
+        'email' : email,
+        'hashpass' : '515151',
+        'name' : 'Wyatt' + str(i),
+        'year' : 1,
+        'isStudentLeader' : 'false',
+        'gender' : 'male'
 
-response = requests.post('http://localhost:3100/create_account',
+        }
+    )
+    time.sleep(1)
+    print("Create account: \t" + str(response.content))
+
+
+response = s.get('http://localhost:3100/login',
     data = {
-            'email' : 'wyatt@test.org',
-            'hashpass' : '515151',
-            'name' : 'Wyatt',
-            'year' : 1, 
-            'isStudentLeader' : 'false', 
-            'gender' : 'male'
+            'email' : 'wyatt@gpmail.org',
+            'pass' : '515151',
            }
 )
+print("Login no cookie: \t" + str(response.content))
+
+'''response = s.delete('http://localhost:3100/logout')
+print("logout: \t\t" + str(response.content))
+
+response = s.get('http://localhost:3100/login',
+   data = {
+           'email' : 'wyatt@test.org',
+           'pass' : '515151',
+          }
+)
+print("Login no cookie #2: \t" + str(response.content))
 
 
+response = s.get('http://localhost:3100/login')
+print("Login with cookie: \t" + str(response.content))'''
 
-print(response.content)
+
+#response = s.get('http://localhost:3100/get_user_data')
+#print("Get user data: \t\t" + str(response.content))
+
+s.close()
